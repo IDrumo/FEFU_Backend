e09b71b3d0e9764e75d3398eeeaf3890b2749c18
Drumo <streltcov.id@students.dvfu.ru>
2023-10-05 17:22:45 +1000

    Initial commit

---
+++ main.java
@@ -0,0 +1,43 @@
+public class Main{
+	public static void main(String[] args) {
+		Cat kitty = new Cat("Пася", 10, "Илья");
+		kitty.meow();
+	}
+}
+
+class Cat{
+    private String name;
+    private int age;
+    private String owner;
+    
+    Cat(String name, int age, String owner){
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+    }
+    
+    void get_name(String name){
+        this.name = name;
+    }
+    
+    void get_age(int age){
+        this.age = age;
+    }
+    
+    void meow(){
+        if (age <= 1){
+            System.out.println("мяу");
+        }else {
+            System.out.println("МЯУУУ");
+        }
+    }
+    
+    void squeeze_milk(){
+        System.out.println("облизал молоко");
+    }
+    
+    void jump(){
+        System.out.println("облизал молоко");
+    }
+    
+}
